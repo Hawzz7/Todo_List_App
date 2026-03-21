8aaefd07da653b12f3ceb321c8462c6b7c0452a1
Hawzz7 <vicky.007.cr@gmail.com>
2026-03-21 21:10:22 +0530

    some changes in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
@@ -7,12 +7,31 @@ import todoRoutes from "./routes/todo.route.js";
 
 const app = express();
 
+
+const allowedOrigins = [
+  "http://localhost:5173",
+];
+
 app.use(cors({
-  origin: "http://localhost:5173", // frontend URL
+  origin: function (origin, callback) {
+    // allow requests with no origin (like Postman)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
+
 app.use(express.json());
 app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/todos", todoRoutes);
